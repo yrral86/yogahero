2bf8b3d5eceead529aac40f2bed4df7cac3acd4c
Larry Reaves <larry@yrral.net>
2008-12-23 15:37:07 -0500

    added our new tests to gather script, moved larry-triangle.png to larry-triangle-left.png so it gets picked up by make test_all

--- images/gather.sh
+++ images/gather.sh
@@ -22,3 +22,17 @@ for i in $SCALED_TESTS; do
 	./gather_one.sh $i$j;
     done;
 done
+
+for i in 5000 15000 20000 25000 50000; do
+    ./gather_one.sh ANGLE$i;
+done
+
+for i in 500 1500 2000 2500 3000 3500 4000; do
+    ./gather_one.sh FLOOR$i;
+done
+
+for i in 10 15 20 25; do
+    for j in 15 20 25 30; do
+	./gather_one.sh ALL$i$j;
+    done;
+done
